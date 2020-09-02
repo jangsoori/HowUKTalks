8d880875035b1fd7c754dbc28628fb57d95801e6
Gracjan Kołodziej <gracjan@MBP-Gracjan.lan>
2020-09-02 21:24:31 +0100

    Fix map not resizing

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,15 +3,15 @@ import { connect } from "react-redux";
 import ReactMapGL from "react-map-gl";
 import Popup from "./Popup";
 import "./Map.css";
-import { getAccents } from "../redux/actions";
+import { getAccents, selectAccent } from "../redux/actions";
 import Markers from "./Markers";
 import Preview from "./Preview";
 
-function Map({ data, getAccents }) {
+function Map({ data, getAccents, selectAccent }) {
   //Set map location for UK
   const [viewport, setViewport] = useState({
-    height: "100vh",
-    width: "100vw",
+    // height: "100%",
+    // width: "100%",
     latitude: 54.55,
     longitude: -2.4333,
     zoom: 4,
@@ -22,19 +22,26 @@ function Map({ data, getAccents }) {
   }, []);
 
   return (
-    <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken={process.env.REACT_APP_ACCESS_TOKEN}
-      onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    >
-      <Markers data={data} />
-      <Preview />
-      <Popup />
-    </ReactMapGL>
+    <div className="map">
+      <ReactMapGL
+        {...viewport}
+        height="100%"
+        width="100%"
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapboxApiAccessToken={process.env.REACT_APP_ACCESS_TOKEN}
+        onViewportChange={(nextViewport) => {
+          setViewport(nextViewport);
+        }}
+      >
+        <Markers data={data} />
+        <Preview />
+      </ReactMapGL>
+      {/* <Popup /> */}
+    </div>
   );
 }
 
 const mapStateToProps = (state) => {
   return { data: state.accents };
 };
-export default connect(mapStateToProps, { getAccents })(Map);
+export default connect(mapStateToProps, { getAccents, selectAccent })(Map);
