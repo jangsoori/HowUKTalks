eec9cce7be0ccdbdb816fa94bf5577c47a9a0cbf
Gracjan Kołodziej <gracjan@MBP-Gracjan.lan>
2020-09-02 23:03:30 +0100

    Added margin and shadow

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,14 @@ import { Menu, Segment, Header } from "semantic-ui-react";
 
 export default function SiteHeader() {
   return (
-    <Menu className="header" style={{}}>
+    <Menu
+      className="header"
+      style={{
+        margin: 0,
+        boxShadow: "0px 5px 5px 0px rgba(0,0,0,0.75)",
+        zIndex: 2,
+      }}
+    >
       <Menu.Item>HowUKTalks</Menu.Item>
     </Menu>
   );
