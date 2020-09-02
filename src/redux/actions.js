e4d10b280b18a236643b485cf193eb8c7fe4fc88
Gracjan Kołodziej <gracjan@MBP-Gracjan.lan>
2020-09-02 17:17:27 +0100

    deleted no longer needed filtering

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,14 +4,9 @@ export const getAccents = () => {
   return async function fetchData(dispatch) {
     const result = await axios.get("./data.json");
     const accents = [...Object.values(result.data.accents)];
-
-    //Get only UK accents
-    const filteredResults = await accents.filter((accent) => {
-      return accent.coords && accent.country === "gb";
-    });
     dispatch({
       type: "GET_ACCENTS",
-      payload: filteredResults,
+      payload: accents,
     });
   };
 };
