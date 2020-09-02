32d57d6b70c077c23742d46bda58d870090faabe
Gracjan Kołodziej <gracjan@MBP-Gracjan.lan>
2020-09-02 21:23:36 +0100

    Fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import "mapbox-gl/dist/mapbox-gl.css";
-
 import Header from "./Header";
 import Map from "./Map";
 import "./App.css";
