21ae8f38ed87b34e0be962f0b33355bba566706d
Gracjan Kołodziej <gracjan@MBP-Gracjan.lan>
2020-09-02 21:25:19 +0100

    Create popup with temporary info

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,20 +3,50 @@ import { connect } from "react-redux";
 import { selectAccent } from "../redux/actions";
 import { Popup as MapPopup } from "react-map-gl";
 import "./Popup.css";
+import { Segment, Grid, Header, Button, Embed } from "semantic-ui-react";
 
 function Popup({ accent, selectAccent }) {
   return (
     accent && (
-      <MapPopup
-        tipSize={5}
-        anchor="top"
-        longitude={accent.coords.lng}
-        latitude={accent.coords.lat}
-        closeOnClick={false}
-        onClose={() => selectAccent(null)}
+      <Segment
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "80%",
+          height: "80%",
+        }}
+        secondary
+        size="massive"
       >
-        {accent.name}
-      </MapPopup>
+        <Grid columns={2} textAlign="center">
+          <Grid.Row>{accent.name}</Grid.Row>
+          <Grid.Row verticalAlign="middle">
+            <Grid.Column>
+              <Header>Trivia</Header>
+            </Grid.Column>
+            <Grid.Column>
+              <Header>Try to understand!</Header>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row verticalAlign="middle">
+            <Grid.Column>hi</Grid.Column>
+            <Grid.Column>
+              <Embed
+                id={accent.videos[0]}
+                placeholder="/images/image-16by9.png"
+                source="youtube"
+              />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row verticalAlign="middle" style={{ height: "100%" }}>
+            <Button onClick={() => selectAccent(null)} color="red">
+              Close
+            </Button>
+          </Grid.Row>
+        </Grid>
+      </Segment>
     )
   );
 }
