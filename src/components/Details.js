7e5664130b733965244175cafa257f43f7d3990e
Gracjan Kołodziej <swider96@gmail.com>
2020-09-03 16:35:57 +0100

    Added temporary links and menu

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,9 +1,14 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import { NavLink, Switch, Route } from "react-router-dom";
 import "./Details.scss";
-import { selectAccent } from "../redux/actions";
+import { selectAccent, getAccent } from "../redux/actions";
+import DetailsWords from "./DetailsWords";
 export const Details = (props) => {
-  const { accent } = props;
+  const { accent, getAccent } = props;
+  useEffect(() => {
+    getAccent(props.match.params.id);
+  }, []);
   if (!accent) {
     return null;
   }
@@ -11,6 +16,23 @@ export const Details = (props) => {
     <div className="accent-details">
       <div className="accent-details-nav">
         <h2 className="accent-details-title">{accent.name}</h2>
+        <NavLink
+          to={`${props.match.url}/words`}
+          className="accent-details-link"
+        >
+          Words
+        </NavLink>
+        <NavLink
+          to={`${props.match.url}/videos`}
+          className="accent-details-link"
+        >
+          Videos
+        </NavLink>
+      </div>
+      <div className="accent-details-content">
+        <Switch>
+          <Route exact path={`/${accent.id}/words`} component={DetailsWords} />
+        </Switch>
       </div>
     </div>
   );
@@ -20,4 +42,4 @@ const mapStateToProps = (state, ownProps) => ({
   accent: state.accents[ownProps.match.params.id],
 });
 
-export default connect(mapStateToProps, { selectAccent })(Details);
+export default connect(mapStateToProps, { selectAccent, getAccent })(Details);
