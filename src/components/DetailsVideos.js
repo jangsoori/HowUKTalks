8549b88dedb67f58350005bbb3b7b79fd66f3a69
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-06 18:20:50 +0100

    Working on changing videos in video tab

--- src/components/DetailsVideos.js
+++ src/components/DetailsVideos.js
@@ -1,30 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import "./DetailsVideos.scss";
 const DetailsVideos = (props) => {
-  if (!props.accent) {
-    return <div className="">loading</div>;
-  }
-  const { videos } = props.accent;
+  const { videos } = props;
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  useEffect(() => {
+    videos && setSelectedVideo(videos[0]);
+  }, []);
 
-  //   if (!videos) {
-  //     return null;
-  //   }
-  console.log(videos);
+  if (!videos) {
+    return null;
+  }
+  console.log(selectedVideo);
   return (
-    <div className="accent-video-wrapper">
-      <iframe
-        className="accent-video"
-        width="420"
-        height="315"
-        src={`https://www.youtube.com/embed/${videos[0]}`}
-      ></iframe>
-    </div>
+    <React.Fragment>
+      <div className="accent-video-wrapper">
+        <iframe
+          className="accent-video"
+          width="420"
+          height="315"
+          src={`https://www.youtube.com/embed/${selectedVideo}`}
+        ></iframe>
+
+        <img
+          onClick={() => setSelectedVideo(videos && videos[1])}
+          src={`https://img.youtube.com/vi/${videos && videos[1]}/0.jpg`}
+          alt=""
+        />
+      </div>
+    </React.Fragment>
   );
 };
 
 const mapStateToProps = (state) => ({
-  accent: state.selectedAccent,
+  videos: state.selectedAccent && state.selectedAccent.videos,
 });
 
 const mapDispatchToProps = {};
