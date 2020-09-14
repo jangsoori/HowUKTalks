9886fefae8d806fe82297b1110e17062e4ff2412
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-09-14 13:02:52 +0100

    Fetch from local json

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,17 +2,17 @@ import axios from "axios";
 
 export const getAccents = () => {
   return async function fetchData(dispatch) {
-    const response = await axios.get("http://localhost:3002/accents");
+    const response = await axios.get("db.json");
     // const accents = [...Object.values(response.data.accents)];
     dispatch({
       type: "GET_ACCENTS",
-      payload: response.data,
+      payload: response.data.accents,
     });
   };
 };
 export const getAccent = (id) => {
   return async function fetchData(dispatch) {
-    const response = await axios.get(`http://localhost:3002/accents/${id}`);
+    const response = await axios.get(`db.json/accents/${id}`);
 
     dispatch({
       type: "GET_ACCENT",
